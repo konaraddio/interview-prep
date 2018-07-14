a27bc572e7c4f66da9425998ffc2be5998aeeb9a
Omkar Konaraddi <omkar.konaraddi@gmail.com>
2018-07-14 14:20:10 -0400

    Using TypeScript + Jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "transform": {
+    "^.+\\.jsx?$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "globals": {
+    "ts-jest": {
+      "useBabelrc": true
+    }
+  }
+}
