c3cd391754a6c4626101b507969907152e708462
konaraddio <omkar.konaraddi@gmail.com>
2018-11-11 18:57:48 -0500

    Simplify quicksort by using last element as pivot

--- src/fundamentals/quicksort/quicksort.ts
+++ src/fundamentals/quicksort/quicksort.ts
@@ -1,38 +1,31 @@
-function quickSort(arr: number[], leftIndex = 0, rightIndex = arr.length - 1) {
-  if (leftIndex < rightIndex) {
-    // in between the partitions (but part of the left partition)
-    const pointOfPartition = partition(arr, leftIndex, rightIndex);
+function quicksort(A, p = 0, r = A.length - 1) {
+  if (p < r) {
+    const q = partition(A, p, r);
+    quicksort(A, p, q - 1);
+    quicksort(A, q + 1, r);
+  }
 
-    if (leftIndex < pointOfPartition - 1) {
-      quickSort(arr, leftIndex, pointOfPartition - 1);
+  function partition(A, p, r) {
+    const pivot = A[r];
+    let i = p - 1;
+    for (let j = p; j < r; j++) {
+      if (A[j] < pivot) {
+        i++;
+
+        // swap
+        let tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
+      }
     }
-    if (pointOfPartition < rightIndex) {
-      quickSort(arr, pointOfPartition, rightIndex);
-    }
+    
+    // swap
+    let tmp = A[i + 1];
+    A[i + 1] = A[r];
+    A[r] = tmp;
+
+    return i + 1
   }
 }
 
-function partition(arr: number[], leftIndex, rightIndex): number {
-  const pivotValue = arr[Math.floor((leftIndex + rightIndex) / 2)];
-
-  while (leftIndex < rightIndex) {
-    while (arr[leftIndex] < pivotValue) {
-      leftIndex++;
-    }
-    while (arr[rightIndex] > pivotValue) {
-      rightIndex--;
-    }
-
-    if (leftIndex <= rightIndex) {
-      const temp = arr[leftIndex];
-      arr[leftIndex] = arr[rightIndex];
-      arr[rightIndex] = temp;
-      leftIndex++;
-      rightIndex--;
-    }
-  }
-
-  return leftIndex;
-}
-
-export default quickSort;
+export default quicksort
