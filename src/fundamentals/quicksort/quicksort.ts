d570be15946460c0b286eeaebff27a8cbca9b0e5
konaraddio <omkar.konaraddi@gmail.com>
2019-01-31 13:54:21 -0500

    Annotate types for quicksort function's parameters

--- src/fundamentals/quicksort/quicksort.ts
+++ src/fundamentals/quicksort/quicksort.ts
@@ -1,11 +1,11 @@
-function quicksort(A, p = 0, r = A.length - 1) {
+function quicksort(A: number[], p = 0, r = A.length - 1) {
   if (p < r) {
     const q = partition(A, p, r);
     quicksort(A, p, q - 1);
     quicksort(A, q + 1, r);
   }
 
-  function partition(A, p, r) {
+  function partition(A: number[], p: number, r: number): number {
     const pivot = A[r];
     let i = p - 1;
     for (let j = p; j < r; j++) {
