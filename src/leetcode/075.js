386bfb8de918e0a9afa0c492496b5fa21342b85c
konaraddi <omkar.konaraddi@gmail.com>
2019-10-03 08:56:52 -0400

    Update solution for Leetcode #75 - Sort Colors with a one-pass solution

--- src/leetcode/075.js
+++ src/leetcode/075.js
@@ -2,6 +2,31 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// Best O(n) - in one pass
+var sortColors = function(nums) {
+  let indexAfter0s = 0;
+  let current = 0;
+  let indexBefore2s = nums.length - 1;
+
+  while (current <= indexBefore2s) {
+    if (nums[current] == 0) {
+      [nums[indexAfter0s], nums[current]] = [nums[current], nums[indexAfter0s]];
+      indexAfter0s++;
+      current++;
+    } else if (nums[current] == 2) {
+      [nums[current], nums[indexBefore2s]] = [
+        nums[indexBefore2s],
+        nums[current]
+      ];
+      indexBefore2s--;
+    } else {
+      current++;
+    }
+  }
+};
+
+// Naive O(n) - uses two passes
+/*
 var sortColors = function(nums) {
   let counts = [0, 0, 0];
 
@@ -17,3 +42,4 @@ var sortColors = function(nums) {
     }
   }
 };
+*/
